42d3b1d5e082342ffbd8ed3c56e0a24d7cec3916
Nick <nick@nickmccrea.com>
2016-11-15 13:10:44 -0500

    Organize session_helper_test.rb.

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -5,11 +5,8 @@ class SessionsHelperTest < ActionView::TestCase
     @user_account = user_accounts( :vader )
   end
 
-  test "function log_in should create a session for the given user account and log it in" do
-    assert_nil session[:user_account_id]
-    log_in( @user_account )
-    assert_equal session[:user_account_id], @user_account.id, "User login does not populate session hash with correct value."
-  end
+  ##
+  # #current_user
 
   test "function current_user_account should return the currently logged-in user if any, otherwise nil" do
     assert_nil session[:user_account_id]
@@ -18,6 +15,18 @@ class SessionsHelperTest < ActionView::TestCase
     assert_equal current_user_account, @user_account, "Function does not return the currently logged-in user."
   end
 
+  ##
+  # #log_in
+
+  test "function log_in should create a session for the given user account and log it in" do
+    assert_nil session[:user_account_id]
+    log_in( @user_account )
+    assert_equal session[:user_account_id], @user_account.id, "User login does not populate session hash with correct value."
+  end
+
+  ##
+  # #log_out
+
   test "function log_out should destroy the current user account session and clear the current user" do
     log_in( @user_account )
     assert_equal current_user_account, @user_account
@@ -25,6 +34,9 @@ class SessionsHelperTest < ActionView::TestCase
     assert_nil session[:user_account_id], "Log out did not remove the current user account from the session hash."
     assert_nil current_user_account,      "Log out did not remove the current user account."
   end
+
+  ##
+  # logged_in?
 
   test "function logged_in? should return true if a user is currently logged in, false otherwise" do
     assert_nil session[:user_account_id]
