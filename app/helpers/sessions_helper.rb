6e78daaf63b05779f9dda0a2f6dfe0f08b16784b
Nick <nick@nickmccrea.com>
2016-11-15 13:28:50 -0500

    Organize session_helper.rb.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,20 @@
 module SessionsHelper
 
   ##
+  # Returns the currently logged-in user account, if any.
+  # Returns nil if no user is logged in.
+  # In order to avoid hitting the database with every call, this method memoizes
+  # the result to an instance variable.
+
+  def current_user_account
+    @current_user_account ||= UserAccount.find_by( id: session[:user_account_id] )
+  end
+
+  ##
   # Logs in the given user account.
 
   def log_in( user_account )
     session[:user_account_id] = user_account.id
-  end
-
-  ##
-  # Returns the currently logged-in user account, if any.
-  # Returns nil if no user is logged in.
-  # In order to avoid hitting the database with every call, this method memoizes the result to an instance variable.
-
-  def current_user_account
-    @current_user_account ||= UserAccount.find_by( id: session[:user_account_id] )
   end
 
   ##
