2f4da1e65196f39a03cbab0eb3b99e928284fc85
Nick <nick@nickmccrea.com>
2016-11-16 12:14:23 -0500

    Prevent double logout from raising error.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -4,7 +4,8 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   
   test "should get new at login path" do
     get login_path
-    assert_response :success
+    assert_response :success,       "Visit login path unsucessful."
+    assert_template 'sessions/new', "Visit login path should render sessions new page."
   end
 
 end
--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -8,8 +8,7 @@ class UserLoginTest < ActionDispatch::IntegrationTest
 
   test "user submits invalid credentials at log in" do
     get login_path
-    assert_response :success,       "Visit login path unsucessful."
-    assert_template 'sessions/new', "Visit login path should render sessions new page."
+    assert_not is_logged_in?,       "No user should be logged in by default."
 
     post login_path, params: { session: { email: "", password: "" } }
     assert_response :success,       "Invalid login submission should generate an HTTP SUCCESS response."
@@ -19,8 +18,6 @@ class UserLoginTest < ActionDispatch::IntegrationTest
 
   test "user submits valid credentials at login and then logs out" do
     get login_path
-    assert_response :success,       "Visit login path unsucessful."
-    assert_template 'sessions/new', "Visit login path should render sessions new page."
     assert_not is_logged_in?,       "No user should be logged in by default."
 
     post login_path, params: { session: { email: @user_account.email, password: @password } }
@@ -34,6 +31,26 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url,            "Visiting the logout path should redirect to the application homepage."
     follow_redirect!
     assert_template 'static_pages/homepage',  "Visiting the logout path should render the application homepage."
+  end
+
+  test "user submits valid credentials in two browsers and then logs out from both browsers" do
+    get login_path
+    assert_not is_logged_in?, "No user should be logged in by default."
+
+    post login_path, params: { session: { email: @user_account.email, password: @password } }
+    assert is_logged_in?,     "Successful login should log the user in immediately."
+    follow_redirect!
+
+    post login_path, params: { session: { email: @user_account.email, password: @password } }
+    assert is_logged_in?,     "Successful login should log the user in immediately."
+    follow_redirect!
+
+    delete logout_path
+    assert_not is_logged_in?, "Visiting the logout path should log the user out immediately."
+    follow_redirect!
+
+    delete logout_path
+    follow_redirect!
   end
 
   test "email is not case-sensitive at login" do
