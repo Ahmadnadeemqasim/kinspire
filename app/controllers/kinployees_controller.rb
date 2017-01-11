6a4178191426c3710b4e0f1d6a05d6cabb15f0f2
Nick <nick@nickmccrea.com>
2017-01-11 15:59:57 -0500

    Improve readability.

--- app/controllers/kinployees_controller.rb
+++ app/controllers/kinployees_controller.rb
@@ -7,14 +7,15 @@ class KinployeesController < ApplicationController
   def create
     # Fix attribute formats.
     kp = kinployee_params
-    kp[:cultural_backgrounds].delete( "" )                  # Collection checkboxes always include an empty string.
-    kp[:languages] = kp[:languages].split( ',' )            # This is entered as a single string by the user, but should be an array of strings.
-                                   .map{ |s| s.strip }
-                                   .map{ |s| s.downcase }
-    kp[:location] = { city:  kp.delete( :city ),            # This is entered as two separate strings but should be a single hash containing the two strings.
-                      state: kp.delete( :state ) }
-    kp[:skills].delete( "" )                                # Collection checkboxes always include an empty string.
-
+    kp[:cultural_backgrounds].delete( "" )  # Collection checkboxes always include an empty string.
+    kp[:languages] = kp[:languages]         # This is entered as a single string by the user, but should be an array of strings.
+      .split( ',' )
+      .map{ |s| s.strip }
+      .map{ |s| s.downcase }
+    kp[:location] = {                       # This is entered as two separate strings but should be a single hash containing the two strings.
+      city:  kp.delete( :city ),
+      state: kp.delete( :state ) }
+    kp[:skills].delete( "" )                # Collection checkboxes always include an empty string.
 
     @kinployee = Kinployee.new( kp )
     @user = User.new( user_params )
@@ -38,24 +39,30 @@ class KinployeesController < ApplicationController
     # Define what paramaters may be mass-assigned to Kinployee objects.
 
     def kinployee_params
-      params.require( :user )
-              .require( :kinployee )
-                .permit(  :availability,
-                          { cultural_backgrounds: [] },
-                          :culture_match_preference,
-                          :languages,
-                          :city,
-                          :state,
-                          :sex,
-                          { skills: [] })
+      params
+        .require( :user )
+        .require( :kinployee )
+        .permit(
+          :availability,
+          { cultural_backgrounds: [] },
+          :culture_match_preference,
+          :languages,
+          :city,
+          :state,
+          :sex,
+          { skills: [] } )
     end
 
     ##
     # Define what parameters may be mass-assigned to User objects.
 
     def user_params
-      params.require( :user ).permit( :email,
-                                      :password, :password_confirmation,
-                                      :role )
+      params
+        .require( :user )
+        .permit(
+          :email,
+          :password, :password_confirmation,
+          :role )
     end
+
 end
